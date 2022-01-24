cb7ca4061858bb06edefee3beab8f675e7091da8
dongyun <hoodiesparrow@gmail.com>
2022-01-25 02:24:20 +0900

    220125 MongoDB

---
+++ database/MongoDB/mongoDB 기초부터 실무까지(feat. Node.js)/mongoDB/src/routes/blogRoute.js
@@ -0,0 +1,85 @@
+const { Router } = require('express')
+const res = require('express/lib/response')
+const { isValidObjectId } = require('mongoose')
+const blogRouter = Router()
+const { Blog } = require('../models/Blog')
+const { User } = require('../models/User')
+
+blogRouter.post('/', async(req, res) => {
+  try {
+    const { title, content, isLive, userId } = req.body
+    if (typeof title !== 'string') 
+      return res.status(400).send({ error: 'title is required' });
+    if (typeof content !== 'string') 
+      return res.status(400).send({ error: 'content is required' });
+    if (isLive && typeof isLive !== 'boolean') 
+      return res.status(400).send({ error: 'isLive must be a boolean' });
+    if (typeof userId !== 'string') 
+      return res.status(400).send({ error: 'user is required' });
+    if (!isValidObjectId(userId)) 
+      return res.status(400).send({ error: 'user ID is invalid' });
+
+    let user = await User.findById(userId)
+    if (!user) 
+      return res.status(400).send({ error: 'user does not exist' });
+    
+    let blog = new Blog({ ...req.body, user: userId })
+    await blog.save()
+    return res.send({ blog })
+  } catch(err) {
+    console.log(err)
+    return res.status(500).send({ error: err.message })
+  }
+})
+
+blogRouter.get('/', async(req, res) => {
+  try {
+    const blogs = await Blog.find({})
+    return res.send({ blogs })
+  } catch(err) {
+    console.log(err)
+    return res.status(500).send({ error: err.message })
+  }
+})
+
+blogRouter.get('/:blogId', async(req, res) => {
+  try {
+    const { blogId } = req.params
+    if (!isValidObjectId(blogId))
+      return res.status(400).send({ error: 'blog ID is invalid' });
+    const blog = await Blog.findOne({ _id: blogId })
+    return res.send({ blog })
+  } catch(err) {
+    console.log(err)
+    return res.status(500).send({ error: err.message })
+  }
+})
+
+blogRouter.put('/:blogId', async(req, res) => {
+  try {
+    const { blogId } = req.params
+    if (!isValidObjectId(blogId))
+      return res.status(400).send({ error: 'Blog ID is invalid ' });
+    const { title, content } = req.body
+    if (typeof title !== 'string') 
+      return res.status(400).send({ error: 'title is required' });
+    if (typeof content !== 'string') 
+      return res.status(400).send({ error: 'content is required' });
+    const blog = await Blog.findOneAndUpdate({ _id: blogId }, { title, content }, { new: true })
+    return res.send({ blog })
+  } catch(err) {
+    console.log(err)
+    return res.status(500).send({ error: err.message })
+  }
+})
+
+blogRouter.patch('/:blogId/live', async(req, res) => {
+  try {
+
+  } catch(err) {
+    console.log(err)
+    return res.status(500).send({ error: err.message })
+  }
+})
+
+module.exports = { blogRouter }
--- database/MongoDB/mongoDB 기초부터 실무까지(feat. Node.js)/mongoDB/src/server.js
+++ database/MongoDB/mongoDB 기초부터 실무까지(feat. Node.js)/mongoDB/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const app = express()
 const { userRouter } = require('./routes/userRoute')
+const { blogRouter } = require('./routes/blogRoute')
 const mongoose = require('mongoose')
 const MONGO_URI = 'mongodb+srv://admin:gyfhUCt8q8BwFjIK@mongodbtutorial.wuvui.mongodb.net/BlogService?retryWrites=true&w=majority'
 
@@ -11,6 +12,7 @@ const server = async() => {
     
     app.use(express.json())    
     app.use('/user', userRouter)
+    app.use('/blog', blogRouter)
 
     app.listen(3000, () => console.log('server listening on port 3000'))
   } catch(err) {
