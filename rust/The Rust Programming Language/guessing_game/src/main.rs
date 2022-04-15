bdcbf4b607be36bc611dba64080e2ae98ecc9d40
Patrick <hoodiesparrow@gmail.com>
2022-04-15 23:51:32 +0900

    220415 Rust Book ch.2

--- rust/The Rust Programming Language/guessing_game/src/main.rs
+++ rust/The Rust Programming Language/guessing_game/src/main.rs
@@ -15,6 +15,13 @@ fn main() {
     io::stdin().read_line(&mut guess)
         .expect("Failed to read line");
 
+    let guess: u32 = guess.trim().parse().expect("Please type a number!"); 
+
     println!("You guessed: {}", guess);
-    println!("Secret number: {}", secret_number);
+
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
