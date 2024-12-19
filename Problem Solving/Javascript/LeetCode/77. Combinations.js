56d3834c3b045b0601a90aa4eb86b04b36530a07
김동윤 <77479710+hoodiesparrow@users.noreply.github.com>
2024-12-19 21:59:58 +0900

    241219 LeetCode Combinations

--- Problem Solving/Javascript/LeetCode/77. Combinations.js
+++ Problem Solving/Javascript/LeetCode/77. Combinations.js
@@ -8,25 +8,43 @@
 var combine = function (n, k) {
   const combinations = [];
 
-  function recursive(combination) {
-    if (combination.length === k) {
-      return combinations.push([...combination]);
-    }
+  function getCombination(start, path) {
+    if (path.length === k) return combinations.push([...path]);
 
-    const lastElem = combination.at(-1);
-
-    for (let next = lastElem + 1; next < n + 1; next++) {
-      combination.push(next);
-      recursive(combination);
-      combination.pop();
+    for (let i = start; i < n + 1; i++) {
+      path.push(i);
+      getCombination(i + 1, path);
+      path.pop();
     }
   }
 
-  for (let m = 1; m < n + 1; m++) {
-    recursive([m]);
-  }
+  getCombination(1, []);
 
   return combinations;
 };
+
+// var combine = function (n, k) {
+//   const combinations = [];
+
+//   function recursive(combination) {
+//     if (combination.length === k) {
+//       return combinations.push([...combination]);
+//     }
+
+//     const lastElem = combination.at(-1);
+
+//     for (let next = lastElem + 1; next < n + 1; next++) {
+//       combination.push(next);
+//       recursive(combination);
+//       combination.pop();
+//     }
+//   }
+
+//   for (let m = 1; m < n + 1; m++) {
+//     recursive([m]);
+//   }
+
+//   return combinations;
+// };
 
 combine(5, 3);
