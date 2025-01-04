934494e6bfb26c58fb0f0b8a87a57fc7955b56bc
김동윤 <77479710+hoodiesparrow@users.noreply.github.com>
2025-01-04 17:56:26 +0900

    250104 LeetCode Permutations

--- Problem Solving/Javascript/LeetCode/46. Permutations.js
+++ Problem Solving/Javascript/LeetCode/46. Permutations.js
@@ -6,28 +6,51 @@
  */
 var permute = function (nums) {
   const permutations = [];
-  const visited = Array(nums.length).fill(false);
+  const N = nums.length;
 
-  function permutation(path) {
-    if (path.length === nums.length) {
-      permutations.push(path.map((idx) => nums[idx]));
-      return;
+  function backTrack(start) {
+    if (start === N) {
+      return permutations.push([...nums]);
     }
 
-    for (let i = 0; i < nums.length; i++) {
-      if (visited[i]) continue;
-
-      path.push(i);
-      visited[i] = true;
-      permutation(path);
-      path.pop();
-      visited[i] = false;
+    for (let i = start; i < N; i++) {
+      [nums[start], nums[i]] = [nums[i], nums[start]];
+      backTrack(start + 1);
+      [nums[start], nums[i]] = [nums[i], nums[start]];
     }
   }
 
-  permutation([]);
+  backTrack(0);
 
   return permutations;
 };
+
+// used visited
+
+// var permute = function (nums) {
+//   const permutations = [];
+//   const visited = Array(nums.length).fill(false);
+
+//   function permutation(path) {
+//     if (path.length === nums.length) {
+//       permutations.push(path.map((idx) => nums[idx]));
+//       return;
+//     }
+
+//     for (let i = 0; i < nums.length; i++) {
+//       if (visited[i]) continue;
+
+//       path.push(i);
+//       visited[i] = true;
+//       permutation(path);
+//       path.pop();
+//       visited[i] = false;
+//     }
+//   }
+
+//   permutation([]);
+
+//   return permutations;
+// };
 
 permute([1, 2, 3]);
